7448b3d7a714599800a3603d24000f4594a6cc7b
JesseMurah <kwekubrown234@gmail.com>
2022-12-29 15:29:26 -0800

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,7 +15,10 @@ int main(void)
 	while (k + j < 4000000)
 	{
 	k += j;
-	if (k % 2 == 0)									sum += k;									j = k - j;									++i;
+	if (k % 2 == 0)
+	{		
+	sum += k;									j = k - j;									++i;
+	}
 	}
 	printf("%ld\n", sum);
 	return (0);
